fb0d4959fd7a66942d0427b4f422081aed1b33b2
fatimasyed1111 <fatima.syed9999@gmail.com>
2025-01-16 23:36:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,14 @@ st.sidebar.write("Office Hours: By Appointment")
 st.subheader("About This Class")
 st.write("This class focuses on practical applications of AI and data science tools, including the use of platforms like Streamlit for project deployment.")
 
+col1, col2 = st.columns(2)
+col1.write('Column 1')
+col2.write('Column 2')
+
+# Three columns with different widths
+col1, col2, col3 = st.columns([3,1,1])
+# col1 is wider
+
+# Using 'with' notation:
+>>> with col1:
+>>>     st.write('This is column 1')
