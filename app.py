4502542f3e7bda47d74fa215ab9d6199e7607146
fatimasyed1111 <fatima.syed9999@gmail.com>
2025-01-16 23:41:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,12 @@ st.sidebar.write("Office Hours: By Appointment")
 st.subheader("About This Class")
 st.write("This class focuses on practical applications of AI and data science tools, including the use of platforms like Streamlit for project deployment.")
 
-col1, col2 = st.columns(2)
-col1.write('Column 1')
-col2.write('Column 2')
+# Insert containers separated into tabs:
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+tab1.write("this is tab 1")
+tab2.write("this is tab 2")
 
-# Three columns with different widths
-col1, col2, col3 = st.columns([3,1,1])
-# col1 is wider
-
-# Using 'with' notation:
-with col1:
-    st.write('This is column 1')
+# You can also use "with" notation:
+with tab1:
+    st.radio("Select one:", [1, 2])
 
